2bfea72e5a00fe385a3c1ffe427ae32d7e41c9f9
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2023-06-25 21:06:14 -0400

    split Recipe component

--- src/renderer/script/components/recipe.tsx
+++ src/renderer/script/components/recipe.tsx
@@ -1,11 +1,12 @@
 import { CList, CObject, CRichText, CVar, InitToken } from "@collabs/collabs";
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 
 import { CollabsQuill } from "../collabs-quill";
 import { useCollab } from "../collabs-react";
-import { reactKey } from "../util/react_key";
-import { CIngredient, Ingredient } from "./ingredient";
+import { CIngredient } from "./ingredient";
+import { RecipeName } from "./recipe_name";
 
+import { Ingredients } from "./ingredients";
 import "./recipe.css";
 
 export class CRecipe extends CObject {
@@ -85,111 +86,16 @@ export class CRecipe extends CObject {
   }
 }
 
-const maxNameLength = 30;
-
 export function Recipe({ recipe }: { recipe: CRecipe }) {
   useCollab(recipe);
 
-  const [nameEditing, setNameEditing] = useState<string | null>(null);
-  const nameValue = nameEditing ?? recipe.recipeName.value;
-
-  // When the local user adds a new ingredient, scroll to it and
-  // select its text.
-  const [newIngr, setNewIngr] = useState<CIngredient | null>(null);
-  const newIngrTextRef = useRef<HTMLInputElement>(null);
-  useEffect(() => {
-    if (newIngrTextRef.current === null) return;
-    newIngrTextRef.current.select();
-    newIngrTextRef.current.scrollIntoView();
-    // Use newIngr as dependency so this only runs on the first render after adding.
-  }, [newIngr]);
-
-  const ingrs = [...recipe.ingredients()];
   return (
     <div className="outerDiv">
-      <div className="recipe-name-wrapper">
-        <input
-          type="text"
-          maxLength={maxNameLength}
-          className="recipe-name"
-          style={{ width: "100%" }}
-          value={nameValue}
-          size={1}
-          onFocus={(e) => e.target.select()}
-          onChange={(e) => setNameEditing(e.target.value)}
-          onBlur={() => {
-            if (nameEditing === null) return;
-            let parsed = nameEditing.slice(0, maxNameLength).trim();
-            if (parsed === "") parsed = "Untitled";
-            recipe.recipeName.value = parsed;
-            setNameEditing(null);
-          }}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") e.currentTarget.blur();
-          }}
-        />
-        {/* Invisible div to force the parent's (hence input's) size to match valueName. */}
-        <div className="recipe-name hidden">{nameValue}</div>
-      </div>
-      {/* Viewport for absolutely-positioned elements inside (class "split"). */}
-      <div style={{ position: "relative", flex: "1 0 auto", width: "100%" }}>
+      <RecipeName recipeName={recipe.recipeName} />
+      <div className="splitViewport">
         <div className="split left">
           <div className="centered">
-            <div className="title">Ingredients</div>
-            {ingrs.map((ingr, index) => (
-              // Use ingr "itself" as a React key instead of Position, so that
-              // React remembers component state even during move ops.
-              // TODO: scroll-to-ingredient if the one you're editing is moved.
-              <div key={reactKey(ingr)} className="ingredientWrapper">
-                <div style={{ display: "flex", flexDirection: "column" }}>
-                  <button
-                    style={{ alignSelf: "flex-start" }}
-                    disabled={index === 0}
-                    onClick={() => recipe.moveIngredient(ingr, index - 1)}
-                  >
-                    ↑
-                  </button>
-                  <button
-                    style={{ alignSelf: "flex-start" }}
-                    disabled={index === ingrs.length - 1}
-                    // +2 because we have to hop over ourselves as well.
-                    onClick={() => recipe.moveIngredient(ingr, index + 2)}
-                  >
-                    ↓
-                  </button>
-                </div>
-                <Ingredient
-                  ingr={ingr}
-                  textRef={ingr === newIngr ? newIngrTextRef : undefined}
-                  // TODO: delay keepIngredient slightly, in case of technically
-                  // sequential but psychologically concurrent delete+edit?
-                  onChange={() => recipe.keepIngredient(ingr)}
-                />
-                <button
-                  onClick={() => recipe.deleteIngredient(ingr)}
-                  className="deleteButton"
-                >
-                  X
-                </button>
-              </div>
-            ))}
-            <button
-              onClick={() => {
-                const ingr = recipe.addIngredient();
-                setNewIngr(ingr);
-              }}
-              className="addButton"
-            >
-              +
-            </button>
-            <br />
-            <button onClick={() => recipe.scale(2)} className="scaleButton">
-              Double the recipe!
-            </button>
-            &nbsp;&nbsp;
-            <button onClick={() => recipe.scale(0.5)} className="scaleButton">
-              Halve the recipe!
-            </button>
+            <Ingredients recipe={recipe} />
           </div>
         </div>
         <div className="split right">
